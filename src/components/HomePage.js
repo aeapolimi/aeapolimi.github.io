3e477b8fa12d6aa170f7e14d97ba1cf6d7acaad3
giulio <giulio09@marcov.it>
2020-09-27 20:31:35 +0000

    Ok per font

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,10 +4,10 @@ import './homePage.css';
 function HomePage (){
     return(<>
             <div className="App">
-                {/* <iframe title="bg" id="bg" src='../../robotrender.html' frameBorder="0"/> */}
-                <iframe title="bg" id="bg" src='https://threejs.org/examples/webgl_loader_collada_kinematics.html' frameBorder="0"/> 
+                <iframe title="bg" id="bg" src='../../robotrender.html' frameBorder="0"/>
+                {/* <iframe title="bg" id="bg" src='https://threejs.org/examples/webgl_loader_collada_kinematics.html' frameBorder="0"/>  */}
                 <header className="App-header">
-                    <div className="rectangle" key="titolo"><span>AEA</span></div>
+                    <div className="rectangle" key="titolo"><span className="titoloLogo">AEA</span></div>
                 </header>
             </div>
         </>
