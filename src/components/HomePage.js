1b49072809d17b646614e12119589ff64a30ea5b
giulio <giulio09@marcov.it>
2020-09-27 20:14:35 +0000

    Aggiunto three.js

---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './homePage.css';
+
+function HomePage (){
+    return(<>
+            <div className="App">
+                {/* <iframe title="bg" id="bg" src='../../robotrender.html' frameBorder="0"/> */}
+                <iframe title="bg" id="bg" src='https://threejs.org/examples/webgl_loader_collada_kinematics.html' frameBorder="0"/> 
+                <header className="App-header">
+                    <div className="rectangle" key="titolo"><span>AEA</span></div>
+                </header>
+            </div>
+        </>
+    )
+}
+
+export default HomePage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,38 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import './App.css';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+// Import dei vari material-ui
+import { createMuiTheme, ThemeProvider, responsiveFontSizes } from '@material-ui/core/styles';
+
+
+// I miei component
+import HomePage from '../components/HomePage';
+
+// Gatsby
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+let theme = createMuiTheme({
+  palette: {
+    primary: { main: '#ffd000' },
+    secondary: { main: '#40bd47' },
+  },
+  status: {
+    danger: 'orange',
+  },
+});
+theme = responsiveFontSizes(theme);
 
-export default IndexPage
+
+function App() {
+  // L'equivalente del main
+  return (
+      <div>
+        <SEO title="Home" />
+        <ThemeProvider theme={theme}>
+          <HomePage/>
+        </ThemeProvider>
+      </div>
+  );
+}
+
+export default App;
