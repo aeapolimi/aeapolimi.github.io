e8e7655bfaa0b362f54fd3d573a3a5a287eba10b
giulio <giulio09@marcov.it>
2020-10-01 13:49:22 +0000

    Collegamento firebase

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -21,15 +21,27 @@ import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import { FirebaseAppProvider, useFirestoreCollectionData, useFirestore, SuspenseWithPerf } from 'reactfire';
 
 import { navigate } from "gatsby"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCN3qF77x39c9RtTO5_s4QMV3lQ589RdZU",
+    authDomain: "aeapolimiweb.firebaseapp.com",
+    databaseURL: "https://aeapolimiweb.firebaseio.com",
+    projectId: "aeapolimiweb",
+    storageBucket: "aeapolimiweb.appspot.com",
+    messagingSenderId: "252147138104",
+    appId: "1:252147138104:web:cc2a953476b0b77f65b0cd"
+  };
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -74,86 +86,79 @@ const responsive = {
 
 function ArticoloCarousel(props){
     const classes = useStyles();
-    const [expanded, setExpanded] = React.useState(false);
-
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
 
     return (
         <Card className={classes.root}>
-        <CardHeader
-            avatar={
-            <Avatar aria-label="recipe" className={classes.avatar}>
-                R
-            </Avatar>
-            }
-            action={
-            <IconButton aria-label="settings">
-                <MoreVertIcon />
-            </IconButton>
-            }
-            title={props.titolo}
-            subheader="September 14, 2016"
-        />
-        {/* <CardMedia
-            className={classes.media}
-            image="/static/images/cards/paella.jpg"
-            title="Paella dish"
-        /> */}
-        <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
-            This impressive paella is a perfect party dish and a fun meal to cook together with your
-            guests. Add 1 cup of frozen peas along with the mussels, if you like.
-            </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-            {/* <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="share">
-            <ShareIcon />
-            </IconButton> */}
-            <IconButton
-            className={clsx(classes.expand, {
-                [classes.expandOpen]: expanded,
-            })}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-            >
-                <ExpandMoreIcon />
-            </IconButton>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardHeader
+                avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                    A
+                </Avatar>
+                }
+                action={
+                <IconButton aria-label="settings">
+                    <MoreVertIcon />
+                </IconButton>
+                }
+                title={props.titolo}
+                subheader={props.data}
+            />
+            {/* <CardMedia
+                className={classes.media}
+                image="/static/images/cards/paella.jpg"
+                title="Paella dish"
+            /> */}
             <CardContent>
-            <Typography paragraph>Method:</Typography>
-            <Typography paragraph>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                minutes.
-            </Typography>
-            <Typography paragraph>
-                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-            <Typography paragraph>
-                Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                minutes more. (Discard any mussels that don’t open.)
-            </Typography>
-            <Typography>
-                Set aside off of the heat to let rest for 10 minutes, and then serve.
-            </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {props.sommario}
+                </Typography>
             </CardContent>
-        </Collapse>
+            <CardActions disableSpacing>
+                <IconButton
+                className={classes.expand}
+                // onClick={handleExpandClick}
+                aria-label="show more"
+                >
+                    <AddCircleIcon />
+                </IconButton>
+            </CardActions>
         </Card>
     );
+}
+
+function NewsSection(){
+    const newsRef = useFirestore().collection("news");
+    const articoli = useFirestoreCollectionData(newsRef);
+    return(<Carousel
+        swipeable={true}
+        draggable={true}
+        showDots={true}
+        responsive={responsive}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+        autoPlaySpeed={500}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        // deviceType={this.props.deviceType}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+        >
+            {articoli.map(articolo => {
+                return (
+                    <div>
+                        <ArticoloCarousel 
+                        titolo={articolo.titolo} 
+                        sommario={articolo.sommario}
+                        data={articolo.data}
+                        />
+                        </div>
+                )
+            })}
+    </Carousel>)
 }
 
 function HomePage (){
@@ -183,30 +188,11 @@ function HomePage (){
                     </Fab>
                 </div>
                 <div title="news" id="news">
-                    <Carousel
-                        swipeable={false}
-                        draggable={false}
-                        showDots={true}
-                        responsive={responsive}
-                        ssr={true} // means to render carousel on server-side.
-                        infinite={true}
-                        // autoPlay={this.props.deviceType !== "mobile" ? true : false}
-                        autoPlaySpeed={1000}
-                        keyBoardControl={true}
-                        customTransition="all .5"
-                        transitionDuration={500}
-                        containerClass="carousel-container"
-                        removeArrowOnDeviceType={["tablet", "mobile"]}
-                        // deviceType={this.props.deviceType}
-                        dotListClass="custom-dot-list-style"
-                        itemClass="carousel-item-padding-40-px"
-                        >
-                        <div><ArticoloCarousel titolo="1"/></div>
-                        <div><ArticoloCarousel titolo="2"/></div>
-                        <div><ArticoloCarousel titolo="3"/></div>
-                        <div><ArticoloCarousel titolo="4"/></div>
-                        <div><ArticoloCarousel titolo="5"/></div>
-                    </Carousel>
+                    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+                        <SuspenseWithPerf fallback={<p>Caricamento news...</p>} traceId={'load-news'}>
+                            <NewsSection/>
+                        </SuspenseWithPerf>
+                    </FirebaseAppProvider>
                 </div>
                 <div className="about" id="about">
                         <Grid
