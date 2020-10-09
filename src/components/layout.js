371bcfdf65df9316a5b3ba102c4f71785ba35f7d
giulio <giulio09@marcov.it>
2020-10-09 09:05:47 +0000

    Fix privacy policy

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,22 +26,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
         <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
     </>
   )
 }
