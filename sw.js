8bb60798bf78e887d10e2a47b3c5b5a7e5910033
giulio <giulio09@marcov.it>
2020-10-16 17:11:03 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-db544610a2dc9b368119.js"
+    "url": "webpack-runtime-99fb91e0dcd99c01d67e.js"
   },
   {
     "url": "framework-4ae830732e225b42b55a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "90cd79d6a08ecffdd9f3cb047cccd39b"
+    "revision": "a933ef06f4cac7d33721a1ca1bca251c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-780d0321bb4481125aff.js"
