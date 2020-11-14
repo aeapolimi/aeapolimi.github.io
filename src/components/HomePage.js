87b5cc0ddfe7209c0c4cfa0b1f50be40935ae0a9
giulio <giulio09@marcov.it>
2020-11-14 16:24:38 +0000

    Fix colore join us

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -371,7 +371,7 @@ function HomePage (){
                                     </Typography>
                                 </CardContent>
                                 <CardActions>
-                                    <Button component={Link} to="/userpage" size="small"><FormattedMessage id="about.joinus"/></Button>
+                                    <Button component={Link} style={{color:"black"}} to="/userpage" size="small"><FormattedMessage id="about.joinus"/></Button>
                                 </CardActions>
                             </Card>
                         </Grid>
