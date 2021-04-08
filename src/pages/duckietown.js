52202a25d3f9f264b85ecd6b44f4f84a7878a39c
giulio <giulio09@marcov.it>
2021-04-08 21:04:35 +0000

    Fix duckietown page

--- src/pages/duckietown.js
+++ src/pages/duckietown.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import IconButton from '@material-ui/core/IconButton';
+import LinkIcon from '@material-ui/icons/Link';
 
 import { useIntl, Link, FormattedMessage } from "gatsby-plugin-intl"
 
@@ -49,9 +50,16 @@ function Projects() {
               <Typography variant="h4" gutterBottom align="center">
                 <FormattedMessage id="duckietown.sottotitoloduckie" />
               </Typography>
+              <Typography variant="body1" gutterBottom align="center" style={{marginTop:"20px"}}>
+                <IconButton color="primary" aria-label="read_more" component={Link} to="/duckie/getting_started">
+                  <LinkIcon style={{"fill":"#ef6c00"}}/>
+                </IconButton>
+                <a href="https://www.duckietown.org/" style={{color:"#ef6c00"}}>www.duckietown.org</a>
+              </Typography>
               <Typography variant="body1" gutterBottom align="justified">
                 <FormattedMessage id="duckietown.descrizioneduckie" />
               </Typography>
+              <hr style={{marginTop:"40px"}}/>
               <Typography variant="h3" gutterBottom align="left" style={{marginTop:"40px"}}>
                 <FormattedMessage id="duckietown.1_introduzione" />
                 <IconButton color="primary" aria-label="read_more" component={Link} to="/duckie/getting_started">
@@ -70,6 +78,10 @@ function Projects() {
               <Typography variant="subtitle2" gutterBottom align="left">
                 <FormattedMessage id="duckietown.2_sottotitolo" />
               </Typography> */}
+              <hr style={{marginTop:"40px"}}/>
+               <Typography variant="h3" gutterBottom align="center" style={{marginTop:"40px"}}>
+                Join our team:
+              </Typography>
               <iframe width="100%" height="480px" src= "https://forms.office.com/Pages/ResponsePage.aspx?id=z6hZgX372UeNMEsbTqkLe9qaZb8UCbFBq16bhK-qrq5UQjgyT1dVMjlXMEpTOTA5U0tWTUpRT0RZRS4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style={{border: "none", maxWidth:"100%", maxHeight:"100vh"}} allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
             </div>
         </Layout>
