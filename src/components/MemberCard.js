70165cc9f4276614df48b2f923c43a676d920916
giulio <giulio09@marcov.it>
2021-04-27 10:30:50 +0000

    Tessera nuova

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -1,32 +1,44 @@
 import React from 'react';
 import { Page, Text, View, Document, StyleSheet, Image } from '@react-pdf/renderer';
 
-import logo from "../images/AEAtondo_trasparente.png"
+import logo from "../images/tessera_AEA.png"
+
+import 'fontsource-roboto';
 
 // Create styles
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'row',
-    // backgroundColor: '#ffe5c0'
+    backgroundColor: 'black'
   },
   section: {
     margin: 10,
     padding: 10,
     flexGrow: 1,
-    marginTop: "130px"
+    marginTop: "85px",
+    position: "absolute",
+    marginLeft: "200px",
+    color: "white",
+    textTransform: "uppercase",
+    textDecoration: "underline",
+    // fontSize: "20px"
+  },
+  view: {
+    width: '100%',
+    height: '100%',
+    padding: 0,
   },
   image: {
-    width: '60%',
-    padding: 10,
-  },
+    objectFit: 'cover',
+  }
 });
 
 // Create Document Component
 const MemberCard = (props) => (
   <Document>
-    <Page size="A6" style={styles.page} orientation="landscape">
-        <View style={styles.image}>
-            <Image src={logo} />
+    <Page size="A6" style={styles.page} orientation="landscape" object-fit="fill">
+        <View style={styles.view}>
+            <Image src={logo} style={styles.image}/>
         </View>
         <View style={styles.section}>
             <Text>{props.username}</Text>
