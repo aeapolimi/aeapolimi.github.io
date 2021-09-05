3ac1a32b23ef886206d0ac8d48abba806b5bb64a
giulio <giulio09@marcov.it>
2021-09-05 16:32:40 +0000

    Inizio forum voti corsi

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,22 +2,70 @@ import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-import Paper from '@material-ui/core/Paper';
-import Collapse from '@material-ui/core/Collapse';
 import Typography from '@material-ui/core/Typography';
 
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableRow from '@material-ui/core/TableRow';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import IconButton from '@material-ui/core/IconButton';
 import HomeIcon from '@material-ui/icons/Home';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 
 import { FormattedMessage } from "gatsby-plugin-intl"
+
+const corsi = [
+    { nome: "AGILE PROJECT MANAGEMENT", tab: "TRANSVERSAL SKILLS"},
+    { nome: "POWER SYSTEMS: ETHICAL ISSUES AND SOCIAL IMPLICATIONS", tab: "TRANSVERSAL SKILLS"},
+    { nome: "CRITICAL THINKING", tab: "TRANSVERSAL SKILLS"},
+    { nome: "EMERGING TECHNOLOGIES AND SOCIETAL CHALLENGES", tab: "TRANSVERSAL SKILLS"},
+    { nome: "ENGINEERING METHODS SUPPORTING JUSTICE", tab: "TRANSVERSAL SKILLS"},
+    { nome: "ETHICS FOR TECHNOLOGY", tab: "TRANSVERSAL SKILLS"},
+    { nome: "HISTORY OF INDUSTRIAL TECHNOLOGICAL INNOVATION", tab: "TRANSVERSAL SKILLS"},
+    { nome: "IMPLICAZIONI LEGALI DELL'ESERCIZIO DELLA PROFESSIONE (LE RESPONSABILITA' DELL'INGEGNERE)", tab: "TRANSVERSAL SKILLS"},
+    { nome: "PATENTS AND INTELLECTUAL PROPERTY MANAGEMENT", tab: "TRANSVERSAL SKILLS"},
+    { nome: "PERSONALITÀ, TEAM BUILDING, LEADERSHIP", tab: "TRANSVERSAL SKILLS"},
+    { nome: "AUTOMATION AND CONTROL IN AUTONOMOUS VEHICLES", tab: "TAB1"},
+    { nome: "CONSTRAINED NUMERICAL OPTIMIZATION FOR ESTIMATION AND CONTROL", tab: "TAB1"},
+    { nome: "CONTROL OF INDUSTRIAL ROBOTS", tab: "TAB1"},
+    { nome: "CONTROL OF MOBILE ROBOTS", tab: "TAB1"},
+    { nome: "DATA DRIVEN CONTROL SYSTEM DESIGN", tab: "TAB1"},
+    { nome: "NETWORKED CONTROL", tab: "TAB1"},
+    { nome: "NONLINEAR CONTROL", tab: "TAB1"},
+    { nome: "NUMERICAL ANALYSIS", tab: "TAB1"},
+    { nome: "ROBUST CONTROL", tab: "TAB1"},
+    { nome: "SAFETY IN AUTOMATION SYSTEMS", tab: "TAB1"},
+    { nome: "SYSTEMS THEORY (NONLINEAR DYNAMICS)", tab: "TAB1"},
+    { nome: "VIBRATION CONTROL AND DIAGNOSTICS OF MECHANICAL SYSTEMS", tab: "TAB1"},
+    { nome: "CONTROL OF INDUSTRIAL AND MOBILE ROBOTS", tab: "TAB1"},
+    { nome: "AUTOMATION AND CONTROL IN VEHICLES", tab: "TAB1"},
+    { nome: "ADVANCED MEASUREMENT SYSTEMS FOR CONTROL APPLICATIONS", tab: "TAB2"},
+    { nome: "ADVANCED PROCESS CONTROL", tab: "TAB2"},
+    { nome: "AUTOMATION AND CONTROL IN ELECTRIC AND HYBRID VEHICLES", tab: "TAB2"},
+    { nome: "AUTOMATION OF ENERGY SYSTEMS", tab: "TAB2"},
+    { nome: "HIGH-TECH ENTREPRENEURSHIP", tab: "TAB2"},
+    { nome: "POWER ELECTRONICS AND SUPPLIESP", tab: "TAB2"},
+    { nome: "PRODUCTION SYSTEMS CONTROL", tab: "TAB2"},
+    { nome: "PROJECT WORK", tab: "TAB2"},
+    { nome: "SIMULATION TECHNIQUES AND TOOLS", tab: "TAB2"},
+    { nome: "MODELLING AND CONTROL OF ENERGY SYSTEMS (C.I.)", tab: "TAB2"},
+    { nome: "ADDITIVE MANUFACTURING", tab: "TAB3"},
+    { nome: "ASSET LIFE CYCLE MANAGEMENT", tab: "TAB3"},
+    { nome: "COMPLESSITÀ NEI SISTEMI E NELLE RETI", tab: "TAB3"},
+    { nome: "ELECTRONIC SYSTEMS", tab: "TAB3"},
+    { nome: "ICT FOR CONTROL SYSTEMS ENGINEERING", tab: "TAB3"},
+    { nome: "IMAGE ANALYSIS AND COMPUTER VISION", tab: "TAB3"},
+    { nome: "MULTIAGENT SYSTEMS", tab: "TAB3"},
+    { nome: "OPTICAL MEASUREMENTS", tab: "TAB3"},
+    { nome: "POWER ELECTRONICS", tab: "TAB3"},
+    { nome: "SISTEMI PER L'AUTOMAZIONE E LA COMUNICAZIONE INDUSTRIALE", tab: "TAB3"},
+    { nome: "DISCRETE OPTIMIZATION", tab: "TAB4"},
+    { nome: "DYNAMICS AND CONTROL OF CHEMICAL PROCESSES", tab: "TAB4"},
+    { nome: "FORMAL METHODS FOR CONCURRENT AND REAL-TIME SYSTEMS (UIC 545)", tab: "TAB4"},
+    { nome: "INTERNET OF THINGS", tab: "TAB4"},
+    { nome: "MACHINE DESIGN", tab: "TAB4"},
+    { nome: "NATURAL RESOURCES MANAGEMENT", tab: "TAB4"},
+    { nome: "NONLINEAR OPTIMIZATION", tab: "TAB4"},
+    { nome: "PERCEPTION, LOCALIZATION AND MAPPING FOR MOBILE ROBOTS", tab: "TAB4"},
+]
 
 const useStyles = makeStyles((theme) => ({
     tablecontainer:{
@@ -35,9 +83,7 @@ const useStyles = makeStyles((theme) => ({
 
 function Note(props){
     const classes = useStyles();
-    const [state, setState] = React.useState({
-        "automazione":false,
-    })
+    const [selezionato, setSelezionato] = React.useState(null);
     return (
         <div style={{marginTop: "20px"}}>
             <IconButton aria-label="home" style={{color:"white"}} onClick = {() => props.setAppunti(false)}>
@@ -47,34 +93,25 @@ function Note(props){
                 Note
             </Typography>
 
-            <div style={{height: "20px"}} />
-            <TableContainer component={Paper} elevation={0} className={classes.tablecontainer}>
-                <Table aria-label="collapsible table">
-                    <TableBody>
-                        <TableRow className={classes.tableroot}>
-                            <TableCell>
-                                <IconButton aria-label="expand row" size="small" onClick={() => setState({ ...state, "automazione": !state["automazione"] })}>
-                                    {state["automazione"] ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                                </IconButton>
-                            </TableCell>
-                            <TableCell component="th" scope="row">
-                                <Typography variant="h4" component="h5" style={{color:"white"}}>
-                                    Automazione
-                                </Typography>       
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                                <Collapse in={state["automazione"]} timeout="auto" unmountOnExit>
-                                    <Typography variant="p" component="p" style={{color:"white"}}>
-                                        <FormattedMessage id="note.automazione" />
-                                    </Typography>   
-                                </ Collapse>
-                            </ TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            <div style={{marginTop: "20px", display: "inline-block"}} >
+                <Autocomplete
+                    id="combo-box-demo"
+                    options={corsi}
+                    groupBy={(option) => option.tab}
+                    getOptionLabel={(option) => option.nome}
+                    style={{ width: "90vw" }}
+                    onChange={(event, newValue) => {
+                        setSelezionato(newValue);
+                      }}
+                    renderInput={(params) => <TextField {...params} label="Courses" variant="outlined" style={{color:"white"}} />}
+                />
+
+                <div style={{height: "20px"}} />
+
+                <Typography variant="h4" component="h5">
+                    {selezionato == null ? "" : selezionato.nome.charAt(0) + selezionato.nome.substring(1).toLowerCase()}
+                </Typography>
+            </div>
         </div>
     )
 }
